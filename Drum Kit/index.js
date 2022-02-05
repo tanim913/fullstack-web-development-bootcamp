6d116763839b7dbe016928d274c3bcc007f6ea4a
Tanim <42853370+tanim913@users.noreply.github.com>
2022-02-05 15:40:13 +0600

    adding keyboard event listener

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,53 +1,63 @@
 
+function keySelected(key) {
+
+    switch (key) {
+            
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            var tom1 = new Audio('sounds/tom-2.mp3');
+            tom1.play();
+            break;
+
+        case "s":
+            var tom1 = new Audio('sounds/tom-3.mp3');
+            tom1.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio('sounds/tom-4.mp3');
+            tom1.play();
+            break;
+
+        case "j":
+            var tom1 = new Audio('sounds/snare.mp3');
+            tom1.play();
+            break;
+
+        case "k":
+            var tom1 = new Audio('sounds/kick-bass.mp3');
+            tom1.play();
+            break;
+
+        case "l":
+            var tom1 = new Audio('sounds/crash.mp3');
+            tom1.play();
+            break;
+
+        default:
+            console.log(buttonInnerHtml);
+            break;
+    }
+    
+}
+
+
+
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
         
         var buttonInnerHtml = this.innerHTML;
 
-        switch (buttonInnerHtml) {
-            
-            case "w":
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-
-            case "a":
-                var tom1 = new Audio('sounds/tom-2.mp3');
-                tom1.play();
-                break;
-
-            case "s":
-                var tom1 = new Audio('sounds/tom-3.mp3');
-                tom1.play();
-                break;
-
-            case "d":
-                var tom1 = new Audio('sounds/tom-4.mp3');
-                tom1.play();
-                break;
-
-            case "j":
-                var tom1 = new Audio('sounds/snare.mp3');
-                tom1.play();
-                break;
-
-            case "k":
-                var tom1 = new Audio('sounds/kick-bass.mp3');
-                tom1.play();
-                break;
-
-            case "l":
-                var tom1 = new Audio('sounds/crash.mp3');
-                tom1.play();
-                break;
-
-            default:
-                console.log(buttonInnerHtml);
-                break;
-        }
+        keySelected(buttonInnerHtml);
         
     })
 }
 
-
+document.addEventListener("keypress", function(event) {
+    keySelected(event.key);
+});
