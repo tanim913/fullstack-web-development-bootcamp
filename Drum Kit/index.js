877f6edf8728dd75d081845cb86e90ed288b7bd8
Tanim <42853370+tanim913@users.noreply.github.com>
2022-02-05 15:25:43 +0600

    adding click function to buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -6,13 +6,44 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++)
         var buttonInnerHtml = this.innerHTML;
 
         switch (buttonInnerHtml) {
+            
             case "w":
                 var tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
                 break;
-            
-        
+
+            case "a":
+                var tom1 = new Audio('sounds/tom-2.mp3');
+                tom1.play();
+                break;
+
+            case "s":
+                var tom1 = new Audio('sounds/tom-3.mp3');
+                tom1.play();
+                break;
+
+            case "d":
+                var tom1 = new Audio('sounds/tom-4.mp3');
+                tom1.play();
+                break;
+
+            case "j":
+                var tom1 = new Audio('sounds/snare.mp3');
+                tom1.play();
+                break;
+
+            case "k":
+                var tom1 = new Audio('sounds/kick-bass.mp3');
+                tom1.play();
+                break;
+
+            case "l":
+                var tom1 = new Audio('sounds/crash.mp3');
+                tom1.play();
+                break;
+
             default:
+                console.log(buttonInnerHtml);
                 break;
         }
         
