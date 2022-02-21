5b7522e1486a55e1abf7d338d3b3ff3f3bdc2213
Tanim <42853370+tanim913@users.noreply.github.com>
2022-02-21 20:10:30 +0600

    initial commit

---
+++ news-letter-signup/app.js
@@ -0,0 +1,7 @@
+const express = require ("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000.");
+})
