2b48ff28ce6d2dffcfc8d8f79f3804ca9722365d
Tanim <42853370+tanim913@users.noreply.github.com>
2022-06-16 20:25:14 +0600

    Update Fibonacci.js

--- JavaScript/Fibonacci.js
+++ JavaScript/Fibonacci.js
@@ -1,29 +1,22 @@
+function fib(n) {
+  var a = 0;
+  var b = 1;
+  var f = [a, b];
 
-
-function fib (n)
-{
-    var a = 0;
-    var b = 1;
-    var f = [a,b];
-    
-    if(n === 1)
-    {
-        var f1 = [];
-        f1[0] = f[0];
-        return f1;
+  if (n === 1) {
+    var f1 = [];
+    f1[0] = f[0];
+    return f1;
+  } else {
+    while (n != 2) {
+      var c = a + b;
+      f.push(c);
+      a = b;
+      b = c;
+      n--;
     }
-    else
-    {
-        while (n!=2)
-        {
-            var c = a+b;
-            f.push(c);
-            a = b;
-            b = c;
-            n--;
-        }
     return f;
-    }
+  }
 }
 //driver code
 console.log(fib(245));
