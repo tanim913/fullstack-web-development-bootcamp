7c05a56ff46709715040572d3db037a208eac6d9
Tanim <42853370+tanim913@users.noreply.github.com>
2022-06-23 00:11:52 +0600

    changes

--- JavaScript/DOM/js/index.js
+++ JavaScript/DOM/js/index.js
@@ -1,4 +1,23 @@
-let val;
-val = this;
-val = document.all[0];
-console.log(val);
+//console.log("hey");
+
+let list = document.getElementsByClassName("container");
+let list2 = document.getElementsByTagName("li");
+let val = document.querySelector("h1");
+let val2 = document.getElementById("x");
+//let list3 = document.querySelectorAll();
+
+let lis = Array.from(list2);
+lis.forEach(function (item) {
+  console.log(item);
+});
+
+console.log(list[0]);
+list[0].style.background = "grey";
+
+let liOdd = document.querySelectorAll("li: nth-child(odd)");
+let liEve = document.querySelectorAll("li: nth-child(even)");
+
+liOdd.forEach(function (item) {
+  item.style.background = "black";
+  item.style.background = "white";
+});
